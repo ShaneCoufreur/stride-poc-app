af171be1e71d8fd9611e7c12f500b754a85cebce
Luke Vance <lukevance90@gmail.com>
2017-10-30 23:43:04 -0700

    got skeleton app running

--- ce-crm-app/app.js
+++ ce-crm-app/app.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
 const http = require('http');
+const expressLib = require('express');
+const bodyParser = require('body-parser');
 const request = require('request');
 const express = expressLib();
 
@@ -15,6 +17,47 @@ app.clientSecret = process.env.CLIENT_SECRET;
 app.environment = process.env.ENV ? process.env.ENV : "production";
 
 const stride = require('./stride')(app);
+
+const getJWT = (req) => {
+    //Extract the JWT token from the request
+    //Either from the "jwt" request parameter
+    //Or from the "authorization" header, as "Bearer xxx"
+    var encodedJwt = req.query['jwt'] ||
+        req.headers['authorization'].substring(7) ||
+        req.headers['Authorization'].substring(7);
+
+    // Decode the base64-encoded token, which contains the context of the call
+    var decodedJwt = jwtUtil.decode(encodedJwt, null, true);
+
+    var jwt = { encoded: encodedJwt, decoded: decodedJwt };
+    return jwt;
+};
+
+const validateJWT = (req, res, next) => {
+    try {
+
+        var jwt = getJWT(req);
+
+        var conversationId = jwt.decoded.context.resourceId;
+        var cloudId = jwt.decoded.context.cloudId;
+        var userId = jwt.decoded.sub;
+
+        // Validate the token signature using the app's OAuth secret (created in DAC App Management)
+        // (to ensure the call comes from Stride)
+        jwtUtil.decode(jwt.encoded, app.clientSecret);
+
+        //all good, it's from Stride, add the context to a local variable
+        res.locals.context = { cloudId: cloudId, conversationId: conversationId, userId: userId };
+
+        // Continue with the rest of the call chain
+        console.log('Valid JWT');
+        next();
+    } catch (err) {
+        console.log('Invalid JWT');
+        res.sendStatus(403);
+    }
+};
+
 
 express.post('/bot-mention',
     validateJWT,
